bbcee5c066895fb08e2928a9b7655275e209d95a
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-05-01 13:03:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
 import joblib
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
 
 # Load the SVM model
 model = joblib.load("models/diabetes_model.joblib")
