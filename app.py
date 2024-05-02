f4224d3b1d43edb091c9153a9c1226056d5508f4
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-05-02 21:57:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,10 +104,10 @@ if __name__ == '__main__':
                
             pdf_bytes = generate_report(Name, prediction, test_data_accuracy)
 
-            if st.button("Download Report"):
-                pdf_base64 = base64.b64encode(pdf_bytes).decode('utf-8')
-                pdf_display = f'<a href="data:application/pdf;base64,{pdf_base64}" download="diabetes_report.pdf">Download Report</a>'
-                st.markdown(pdf_display, unsafe_allow_html=True)
+            
+            pdf_base64 = base64.b64encode(pdf_bytes).decode('utf-8')
+            pdf_display = f'<a href="data:application/pdf;base64,{pdf_base64}" download="diabetes_report.pdf">st.button("Download Report")</a>'
+            st.markdown(pdf_display, unsafe_allow_html=True)
            
 
     if selected == "Our Prediction Records":
